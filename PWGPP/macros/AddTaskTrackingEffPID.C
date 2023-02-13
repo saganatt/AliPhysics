1acb87f2621c10d4d0b1375c650e4a4d87835306
Maja Kabus <mkabus@cern.ch>
2023-02-13 12:48:54 +0100

    Fix logs in eff pid

--- PWGPP/macros/AddTaskTrackingEffPID.C
+++ PWGPP/macros/AddTaskTrackingEffPID.C
@@ -17,13 +17,13 @@ AliAnalysisTaskTrackingEffPID* AddTaskTrackingEffPID(TString suffix = "",
   // Get the current analysis manager
   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
   if (!mgr) {
-    Error("AddTaskLFefficiencies", "No analysis manager found.");
+    Error("AddTaskEffPID", "No analysis manager found.");
     return 0x0;
   }
 
   // Check the analysis type using the event handlers connected to the analysis manager.
   if (!mgr->GetInputEventHandler()) {
-    ::Error("AddTaskLFefficiencies", "This task requires an input event handler");
+    ::Error("AddTaskEffPID", "This task requires an input event handler");
     return 0x0;
   }
   
