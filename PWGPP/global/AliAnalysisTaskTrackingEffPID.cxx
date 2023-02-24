1e260af2264e43826bac5350f1be09908618b3c7
Maja Kabus <mkabus@cern.ch>
2023-02-24 17:04:46 +0100

    Change eta limits in the main eff task

--- PWGPP/global/AliAnalysisTaskTrackingEffPID.cxx
+++ PWGPP/global/AliAnalysisTaskTrackingEffPID.cxx
@@ -174,8 +174,8 @@ void AliAnalysisTaskTrackingEffPID::UserCreateOutputObjects() {
   const int nPtBins=42;
   const int nMultBins=10;
   int nbins[6]={10,18,nPtBins,nMultBins,4,60};
-  double xmin[6]={-1.,0.,0.,0,-10.,0.};
-  double xmax[6]={1.,2*TMath::Pi(),30.,200.,10.,3.};
+  double xmin[6]={-0.8,0.,0.,0,-10.,0.};
+  double xmax[6]={0.8,2*TMath::Pi(),30.,200.,10.,3.};
   TString charge[2] = {"pos","neg"};
   double ptBins[nPtBins+1] = {0.00,0.05,0.075,0.10,0.11,0.12,0.13,0.14,0.15,
                               0.16,0.17,0.18,0.19,0.20,0.225,0.25,0.30,0.35,
